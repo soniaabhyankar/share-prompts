c05c93da3146e69a35621cd662e10b24c0db7285
Sonia Abhyankar <abhyankar.sonia@gmail.com>
2023-09-01 15:30:12 +0200

    Add navigation menu

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,7 @@ const Nav = () => {
 	const isUserLoggedIn = true;
 
 	const [providers, setProviders] = useState(null);
+	const [toggleDropdown, setToggleDropdown] = useState(false);
 
 	useEffect(() => {
 		const setupProviders = async () => {
@@ -21,14 +22,14 @@ const Nav = () => {
 	}, []);
 
 	return (
-		<nav className='w-full flex-between mb-16 pt-3'>
-			<Link href='/' className='flex gap-2 flex-center'>
+		<nav className='w-full flex-between mb-16 pt-3 '>
+			<Link href='/' className='flex gap-2 flex-center '>
 				<Image src='/assets/images/logo.svg' width={30} height={30} className='object-contain' alt='Promptopia logo' />
 				<p className='logo_text'>Promptopia</p>
 			</Link>
 
 			{/* Desktop Navigation */}
-			<div className='sm:flex hidden'>
+			<div className='sm:flex hidden '>
 				{isUserLoggedIn ? (
 					<div className='flex gap-3 md:gap-5'>
 						<Link href='/create-prompt' className='black_btn'>
@@ -42,6 +43,52 @@ const Nav = () => {
 						<Link href='/profile'>
 							<Image src='/assets/images/profile.svg' width={37} height={37} className='rounded-full' alt='profile' />
 						</Link>
+					</div>
+				) : (
+					<>
+						{providers &&
+							Object.values(providers).map((provider) => (
+								<button type='button' key={provider.name} onClick={() => signIn(provider.id)} className='black_btn'>
+									Sign In
+								</button>
+							))}
+					</>
+				)}
+			</div>
+
+			{/* Mobile Navigation */}
+			<div className='sm:hidden flex relative '>
+				{isUserLoggedIn ? (
+					<div className='flex '>
+						<Image
+							src='/assets/images/profile.svg'
+							width={37}
+							height={37}
+							className='rounded-full'
+							alt='profile'
+							onClick={() => setToggleDropdown((prevState) => !prevState)}
+						/>
+
+						{toggleDropdown && (
+							<div className='dropdown '>
+								<Link href='/profile' className='dropdown_link' onClick={() => setToggleDropdown(false)}>
+									My Profile
+								</Link>
+								<Link href='/create-prompt' className='dropdown_link' onClick={() => setToggleDropdown(false)}>
+									Create Prompt
+								</Link>
+								<button
+									type='button'
+									onClick={() => {
+										setToggleDropdown(false);
+										signOut();
+									}}
+									className='mt-5 w-full black_btn'
+								>
+									Sign Out
+								</button>
+							</div>
+						)}
 					</div>
 				) : (
 					<>
